1b7ad681999ed8e8a3b80bbe5613be4f262fa4e9
Ekramul Islam Sumon <38817030+sumoninfo@users.noreply.github.com>
2023-06-20 13:15:36 +0600

    query params

--- myapi.py
+++ myapi.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Path
+from typing import Optional
 
 app = FastAPI()
 
@@ -20,3 +21,11 @@ students = {
 @app.get("/get-student/{student_id}")
 def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=3)):
     return students[student_id]
+
+
+@app.get("/get-by-name")
+def get_student(*, name: Optional[str] = None):
+    for student_id in students:
+        if students[student_id]["name"] == name:
+            return students[student_id]
+    return {"Data": "Not found"}
